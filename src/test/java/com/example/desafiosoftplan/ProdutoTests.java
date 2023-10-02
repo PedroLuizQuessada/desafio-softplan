173b169d6b5f2d5d98984360d9cf431449daa0e3
Pedro Quessada <pedroluiz.quessada@gmail.com>
2023-10-02 18:50:36 -0300

    testes para entidade produto

---
+++ src/test/java/com/example/desafiosoftplan/ProdutoTests.java
@@ -0,0 +1,80 @@
+package com.example.desafiosoftplan;
+
+import com.example.desafiosoftplan.exception.ProdutoNaoEncontradoException;
+import com.example.desafiosoftplan.model.Produto;
+import com.example.desafiosoftplan.service.ProdutoService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+@ComponentScan(basePackages = {"com.example.desafiosoftplan"})
+public class ProdutoTests {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Test
+    public void getAllProdutos() {
+        List<Produto> produtos = produtoService.getAllProdutos();
+        Assertions.assertThat(produtos).isNotNull();
+    }
+
+    @Test
+    public void getProdutoById() throws ProdutoNaoEncontradoException {
+        Long id = 1L;
+
+        Produto produto = produtoService.getProdutoById(id);
+        Assertions.assertThat(produto).isNotNull();
+        Assertions.assertThat(produto.getId()).isNotNull();
+    }
+
+    @Test
+    public void addProduto() {
+        String nome = "Mesa";
+        Long qtdadeEstoque = 7L;
+
+        Produto produto = new Produto();
+        produto.setNome(nome);
+        produto.setQtdadeEstoque(qtdadeEstoque);
+
+        Produto produtoSalvo = produtoService.addProduto(produto);
+        Assertions.assertThat(produtoSalvo.getId()).isNotNull();
+        Assertions.assertThat(produtoSalvo.getId()).isGreaterThan(0L);
+    }
+
+    @Test
+    public void updateProduto() throws ProdutoNaoEncontradoException {
+        Long id = 1L;
+        String novoNome = "Mesa de mármore";
+
+        Produto produto = produtoService.getProdutoById(id);
+        produto.setNome(novoNome);
+
+        Produto produtoSalvo = produtoService.updateProduto(produto);
+        Assertions.assertThat(produtoSalvo.getNome()).isNotNull();
+        Assertions.assertThat(produtoSalvo.getNome()).isEqualTo(novoNome);
+    }
+
+    @Test
+    public void deleteProdutoById() throws Exception {
+        Long id = 1L;
+
+        produtoService.getProdutoById(id);
+        produtoService.deleteProdutoById(id);
+        try {
+            produtoService.getProdutoById(id);
+            throw new Exception("Falha ao deletar produto");
+        }
+        catch (ProdutoNaoEncontradoException ignored) {
+
+        }
+    }
+}
