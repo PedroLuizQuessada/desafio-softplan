22735b1a7fdc84d17675c6c32f801fdaf54f617b
Pedro Quessada <pedroluiz.quessada@gmail.com>
2023-10-02 17:52:27 -0300

    commit inicial

---
+++ src/main/java/com/example/desafiosoftplan/DesafioSoftplanApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiosoftplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSoftplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSoftplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafiosoftplan/DesafioSoftplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiosoftplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSoftplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
