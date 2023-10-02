d4ca91f64e721f110cc298956788279023cdd46b
Pedro Quessada <pedroluiz.quessada@gmail.com>
2023-10-02 19:48:43 -0300

    inserindo dados na inicialização

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuarios (username, senha, adm) VALUES ('admin', 'senha123', true);
+INSERT INTO usuarios (username, senha, adm) VALUES ('user', 'senha123', false);
+INSERT INTO produtos (nome, quantidade_estoque) VALUES ('Panela', 6);
